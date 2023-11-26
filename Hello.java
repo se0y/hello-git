1733f699b07ab906deceabe9732d62c3eb08417a
EC2 Default User <ec2-user@ip-172-31-34-80.ap-northeast-2.compute.internal>
2023-11-26 09:32:22 +0000

    added license again

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 // Hello.java
-/* Copyright [2023] [Seoyeon Gong]
+/* Copyright [2023] [SeoYeon Gong]
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
